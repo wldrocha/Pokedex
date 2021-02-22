35793ebb5e7b847afa378f3d38f43b581914e18c
Wladimir Rocha <wld.rocha@gmail.com>
2021-02-22 01:14:30 -0400

    refactoring code

--- app/src/main/java/com/example/pokemon/Activities/MainActivity.kt
+++ app/src/main/java/com/example/pokemon/Activities/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.pokemon.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.pokemon.APIService
 import com.example.pokemon.Adapters.PokemonAdapter
 import com.example.pokemon.Models.Pokemon
@@ -21,15 +23,7 @@ class MainActivity : AppCompatActivity(), PokemonAdapter.OnPokemonClickListener 
     private lateinit var binding:ActivityMainBinding
     private lateinit var adapter: PokemonAdapter
     private val pokemons = mutableListOf<Pokemon>()
-
-    /*val pokemons = listOf(
-             Pokemon(1, "bulbasaur", 1, "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"),
-             Pokemon(2, "ivysaur", 2, "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png"),
-             Pokemon(3, "venusaur", 3, "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png"),
-             Pokemon(4, "charmander", 4, "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"),
-             Pokemon(5, "charmeleon", 5, "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png"),
-             Pokemon(6, "charizard", 6, "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png")
-     )*/
+    private var pokemonsPage: Int = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,10 +33,12 @@ class MainActivity : AppCompatActivity(), PokemonAdapter.OnPokemonClickListener 
     }
 
     private fun initRecyclerView(){
-        this.adapter = PokemonAdapter(this, pokemons, this)
+
         binding.recyclerViewPokemon.layoutManager = LinearLayoutManager(this)
+        this.adapter = PokemonAdapter(this, pokemons, this)
         binding.recyclerViewPokemon.adapter = adapter
-        getAllPokemons()
+        getAllPokemons(pokemonsPage)
+        setRecyclerViewScrollListener()
     }
 
     override fun onItemClickListener(name: String) {
@@ -56,9 +52,11 @@ class MainActivity : AppCompatActivity(), PokemonAdapter.OnPokemonClickListener 
                 .build()
     }
 
-    private fun getAllPokemons(){
+    private fun getAllPokemons(limit: Int){
+        val limitX = limit *  10
+        val offset =  if(limit == 2)  0 else limitX
         CoroutineScope(Dispatchers.IO).launch {
-            val callPokemonsList: Response<PokemonsListResponse> = getRetrofit().create(APIService::class.java).getAllPokemons("pokemon/")
+            val callPokemonsList: Response<PokemonsListResponse> = getRetrofit().create(APIService::class.java).getAllPokemons("pokemon/?offset=$offset&limit=$limitX")
             val listResponseCall: PokemonsListResponse? = callPokemonsList.body()
 
             val pokemonList: List<Pokemon>? = listResponseCall?.pokemons ?: emptyList()
@@ -74,11 +72,30 @@ class MainActivity : AppCompatActivity(), PokemonAdapter.OnPokemonClickListener 
            runOnUiThread {
                 if(callPokemonsList.isSuccessful){
                     adapter.notifyDataSetChanged()
+                    println("pokemonsCount ${pokemons.size}")
                 }else{
                     showError()
                 }
             }
         }
+    }
+
+    private fun setRecyclerViewScrollListener(){
+
+        binding.recyclerViewPokemon.addOnScrollListener(object: RecyclerView.OnScrollListener(){
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+
+                if(!binding.recyclerViewPokemon.canScrollVertically(1)){
+                    pokemonsPage += 1
+                    getAllPokemons(pokemonsPage)
+                }
+                /*if(dy > 0){
+                    val visibleCount = binding.recyclerViewPokemon.childCount
+                    val pastVisibleItem = binding.recyclerViewPokemon.canScrollVertically(1)
+                }*/
+                super.onScrolled(recyclerView, dx, dy)
+            }
+        })
     }
 
     private fun showError(){
